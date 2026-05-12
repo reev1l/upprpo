8322b11afc2079cc37d7c224b400f6031a7ece17
reevl1 <bazdireev.l@gmail.com>
2026-05-12 17:30:33 +0700

    task

---
+++ test-repo/build/CMakeFiles/testproj.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/bazdi/Desktop/UPPRPO/test-repo/candle.cpp" "CMakeFiles/testproj.dir/candle.cpp.obj" "gcc" "CMakeFiles/testproj.dir/candle.cpp.obj.d"
+  "C:/Users/bazdi/Desktop/UPPRPO/test-repo/main.cpp" "CMakeFiles/testproj.dir/main.cpp.obj" "gcc" "CMakeFiles/testproj.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ test-repo/build/CMakeFiles/testproj.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testproj.dir/candle.cpp.obj"
+  "CMakeFiles/testproj.dir/candle.cpp.obj.d"
+  "CMakeFiles/testproj.dir/main.cpp.obj"
+  "CMakeFiles/testproj.dir/main.cpp.obj.d"
+  "libtestproj.dll.a"
+  "testproj.exe"
+  "testproj.exe.manifest"
+  "testproj.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testproj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
